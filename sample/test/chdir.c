3dd57697a8984dd444b1577b49a41804b85d106a
blindacai <linda_cai2014@hotmail.com>
2017-04-03 02:19:23 -0700

    test chdir

--- sample/test/chdir.c
+++ sample/test/chdir.c
@@ -7,6 +7,25 @@
 
 int main(){
     char* directory = "callfun";
-    int result = listFiles(1, directory);
-    printf("list dir: %d\n", result);
+    listFiles(1, directory);
+    //int result = listFiles(1, directory);
+    //printf("list dir: %d\n", result);
+
+    printf("now cd into callfun\n");
+    chdir(directory);
+    char* inner_dir = "innerdir";
+    listFiles(1, "./");
+
+    printf("now cd into innerdir\n");
+    chdir("innerdir");
+    listFiles(1, "./");
+
+    char* dir_notexist = "outerdir";
+    int error = chdir(dir_notexist);
+    printf("cd into non-exist dir: %d\n", error);
+
+    char* to_parent = "../";
+    int parent = chdir(to_parent);
+    printf("cd into parent dir: %d\n", parent);
+    listFiles(1, "./");
 }
